8c14994747f981507e4fecc0e5c233e6e9a8fcf2
Tuấn Anh <92778429+ntacsharp@users.noreply.github.com>
2025-05-17 12:04:07 +0700

    update::first update to README.md

--- bikeshare.py
+++ bikeshare.py
@@ -175,7 +175,6 @@ def trip_duration_stats(df):
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
-
 def user_stats(df):
     """Displays statistics on bikeshare users."""
 
